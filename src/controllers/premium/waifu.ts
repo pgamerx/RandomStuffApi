0a7547b725619805d2d4bdbcd934a91d587c9d3d
PGamerX <websitedesigner123456@gmail.com>
2021-12-25 15:11:45 +0530

    Update waifu.ts

--- src/controllers/premium/waifu.ts
+++ src/controllers/premium/waifu.ts
@@ -39,6 +39,10 @@ const SfwWaifu = async (req: Request, res: Response, next: NextFunction) => {
 }
 
 const NsfwWaifu = async (req: Request, res: Response, next: NextFunction) => {
+      const private_key = process.env.PRIVATE_KEY! as string;
+  const RapidApi = req.get("x-RapidApi-private") || req.get("RapidApi-private")
+  if(RapidApi !== private_key) return res.status(400).send("You are only allowed to make requests through RapidApi, contact for more support.")
+  
     const AuthKey = req.query.Authorization! as string;
     if (!AuthKey)
         return res
