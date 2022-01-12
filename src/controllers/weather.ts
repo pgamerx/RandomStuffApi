3cb46dcaaf7efcf7ec4eeaa0697a6d34f2a2918d
PGamerX <websitedesigner123456@gmail.com>
2022-01-12 18:47:21 +0530

    Set Basic Template: "/weather"

---
+++ src/controllers/weather.ts
@@ -0,0 +1,25 @@
+/** Importing NPM modules */
+import { Request, Response, NextFunction } from "express";
+const fetch = require("node-fetch");
+import { isNormal, isPremium } from "../checkers/plan";
+/** Importing Custom Modules */
+
+/** Importing DotEnv for process.env */
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const GetWeather = async(req: Request, res: Response, next: NextFunction) => {
+    const private_key = process.env.PRIVATE_KEY! as string;
+    const RapidApi = req.get("x-RapidApi-private") || req.get("RapidApi-private")
+    if(RapidApi !== private_key) return res.status(400).send("You are only allowed to make requests through RapidApi, contact for more support.")
+    
+    const AuthKey = req.get("Authorization")! as string;
+    if (!AuthKey)
+      return res
+        .status(403)
+        .send("Authorization key is not present, contact for more support.");
+    if (!await isNormal(AuthKey) && !await isPremium(AuthKey))
+      return res
+        .status(403)
+        .send("Your key is invalid, contact for more support.");
+}
